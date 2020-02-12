04f3646e6bb36986d46b9b16606a15b15976f755
ochoaste <steffieocho@gmail.com>
2020-02-11 19:23:19 -0600

    Static Pages - 0 errors

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,15 @@
 require 'test_helper'
 
-def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
- end
-  
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-	assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -16,11 +17,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
-  
+
   test "should get about" do
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
-
 end
