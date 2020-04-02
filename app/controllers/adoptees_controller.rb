57969e6b96e4ce01346955d0fd65e5b2c09d5712
ochoaste <steffieocho@gmail.com>
2020-04-02 17:31:28 -0500

    Form Basics Done

--- app/controllers/adoptees_controller.rb
+++ app/controllers/adoptees_controller.rb
@@ -3,7 +3,7 @@ class AdopteesController < ApplicationController
 # before_action :logged_in_user, only: [:index, :edit, :update]
 
 def index
-  @adoptees = Adoptee.all 
+ @adoptees = Adoptee.paginate(page: params[:page]) 
  end
 
 def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Adoptee.create!(name:  "Example User",
+	     age: 100, 
+             email: "example@railstutorial.org",
+             personality: "Example", 
+	     password:              "foobar",
+             password_confirmation: "foobar")
+
+50.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  age = 100
+  personality = "personality here" 
+  Adoptee.create!(name:  name,
+               email: email,
+               age: age,
+               personality: personality,
+	       password:              password,
+               password_confirmation: password)
+end
+
