b1c719bf05ea878d6efb06b8662367a3f618281c
ochoaste <steffieocho@gmail.com>
2020-04-07 19:18:02 -0500

    Finished signup

--- test/integration/adoptees_signup_test.rb
+++ test/integration/adoptees_signup_test.rb
@@ -5,5 +5,18 @@ class AdopteesSignupTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'Adoptee.count', 1 do
+      post users_path, params: { adoptee: { name:  "Example User",
+        age: 100,                                  
+	email: "user@example.com",
+	personality: "unknown", 
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'adoptees/show'
+  end
 
 end
